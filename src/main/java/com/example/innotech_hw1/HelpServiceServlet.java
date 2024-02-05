1b9c693083a39cb9e2a4d0a4709b561db875beef
Alex Drozdov <jokend1ng2@yandex.ru>
2024-02-05 19:20:37 +0300

    исправил ошибки

--- src/main/java/com/example/innotech_hw1/HelpServiceServlet.java
+++ src/main/java/com/example/innotech_hw1/HelpServiceServlet.java
@@ -7,28 +7,29 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 
 @WebServlet("/v1/support")
 public class HelpServiceServlet extends HttpServlet {
 
-    private final PhrasesDao words= new PhrasesDao();
+    private final PhrasesDao words = new PhrasesDao();
 
     public PhrasesDao getWords() {
         return words;
     }
 
     @Override
-    public void init(){
+    public void init() {
         words.getWords().add(new Phrase("У тебя все получится"));
     }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        resp.setContentType("text/plain");
         try (var writer = resp.getWriter()) {
             writer.write(words.getRandomPhrase());
+            resp.setContentType("text/plain");
         } catch (IOException e) {
             throw new MyException("Не вывести фразу");
         }
